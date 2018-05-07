04e9938e0d6cce8d9b65115c3020cf164ea02d34
Neon <aion@gmx-topmail.de>
2018-05-07 22:44:55 +0200

    deleted and ignored buildable start files (build via dist-build.xml)

--- AC-Game/dist/Template-StartGS.bat
+++ AC-Game/dist/Template-StartGS.bat
@@ -2,19 +2,10 @@
 TITLE Aion-Core - GameServer Console
 :START
 CLS
-SET JAVAVER=1.7
-SET NUMAENABLE=false
-CLS
 IF "%MODE%" == "" (
 CALL PanelGS.bat
 )
 
-IF "%JAVAVER%" == "1.7" (
-SET JAVA_OPTS=-XX:-UseSplitVerifier -XX:+TieredCompilation %JAVA_OPTS%
-)
-IF "%NUMAENABLE%" == "true" (
-SET JAVA_OPTS=-XX:+UseNUMA %JAVA_OPTS%
-)
 ECHO Starting Aion-Core GameServer in %MODE% mode.
 JAVA %JAVA_OPTS% -ea -javaagent:./libs/ac-commons-1.3.jar -cp ./libs/*;AC-Game.jar com.aionemu.gameserver.GameServer
 SET CLASSPATH=%OLDCLASSPATH%
@@ -23,7 +14,7 @@ IF ERRORLEVEL 1 GOTO ERROR
 IF ERRORLEVEL 0 GOTO END
 :ERROR
 ECHO.
-ECHO GameServer has terminated abnormaly!
+ECHO GameServer has terminated abnormally!
 ECHO.
 PAUSE
 EXIT
